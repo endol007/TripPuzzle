bc466d1cdb8c883f15a4a777a37e6410e15d72ef
swgil007 <48559894+swgil007@users.noreply.github.com>
2019-03-27 18:46:52 +0900

    kakaoCon

---
+++ Gil/BigBaseVer 0.0.1/app/src/main/java/com/example/bigbase/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bigbase;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainintent = new Intent(MainActivity.this, Login.class);
+                MainActivity.this.startActivity(mainintent);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
