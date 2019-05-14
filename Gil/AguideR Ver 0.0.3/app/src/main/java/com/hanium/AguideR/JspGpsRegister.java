de8093484d84792d68b00b1ddbfe8ff5cad4e374
swgil007 <48559894+swgil007@users.noreply.github.com>
2019-05-14 11:47:40 +0900

    aguider 0.0.5
    Login.jsp 연동

--- Gil/AguideR Ver 0.0.3/app/src/main/java/com/hanium/AguideR/JspGpsRegister.java
+++ Gil/AguideR Ver 0.0.3/app/src/main/java/com/hanium/AguideR/JspGpsRegister.java
@@ -96,98 +96,98 @@ public class JspGpsRegister extends Activity {
 
 
         registerbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userID = regid.getText().toString();
-                String userPassword = regpw.getText().toString();
-                String userName = regname.getText().toString();
-                int userAge = Integer.parseInt(regage.getText().toString());
-                String userSex = resultSex;
-                String userPhone = regphone.getText().toString();
-                String userMail = regmail.getText().toString();
-
-
-                gps = new GpsInfo(JspGpsRegister.this);
-                double latitu = gps.getLatitude();
-                double longit = gps.getLongitude();
-
-
-
-                String gpsLati = Double.toString(latitu);
-                String gpsLong = Double.toString(longit);
-
-
-
-
-                try {
-                    String result;
-                    CustomTask task = new CustomTask();
-                    result = task.execute(userID, userPassword, userName, String.valueOf(userAge), userSex, userPhone, userMail, gpsLati, gpsLong).get();
-                    Log.i("리턴 값", result);
-
-                } catch (Exception e) {
-
-                }
-            }
-        });
-
-
-
-
-    }
-
-
-    class CustomTask extends AsyncTask<String, Void, String> {
-        String sendMsg, receiveMsg;
-        @SuppressLint("WrongThread")
         @Override
-        // doInBackground의 매개값이 문자열 배열인데요. 보낼 값이 여러개일 경우를 위해 배열로 합니다.
-        protected String doInBackground(String... strings) {
+        public void onClick(View v) {
+            String userID = regid.getText().toString();
+            String userPassword = regpw.getText().toString();
+            String userName = regname.getText().toString();
+            int userAge = Integer.parseInt(regage.getText().toString());
+            String userSex = resultSex;
+            String userPhone = regphone.getText().toString();
+            String userMail = regmail.getText().toString();
+
+
+            gps = new GpsInfo(JspGpsRegister.this);
+            double latitu = gps.getLatitude();
+            double longit = gps.getLongitude();
+
+
+
+            String gpsLati = Double.toString(latitu);
+            String gpsLong = Double.toString(longit);
+
+
+
+
             try {
-                String str;
-                URL url = new URL("http://182.210.78.154:8080/RegisterGps.jsp");//보낼 jsp 주소를 ""안에 작성합니다.
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                conn.setRequestMethod("POST");//데이터를 POST 방식으로 전송합니다.
-                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-                sendMsg = "&userID="+strings[0]+"&userPassword="+strings[1]+"&userName="+strings[2]+"&userAge="+strings[3]+"&userSex="+strings[4]+"&userPhone="+strings[5]+"&userMail="+strings[6]+"&gpsLati="+strings[7]+"&gpsLong="+strings[8];
+                String result;
+                CustomTask task = new CustomTask();
+                result = task.execute(userID, userPassword, userName, String.valueOf(userAge), userSex, userPhone, userMail, gpsLati, gpsLong).get();
+                Log.i("리턴 값", result);
 
-                //보낼 정보인데요. GET방식으로 작성합니다. ex) "id=rain483&pwd=1234";
-                //회원가입처럼 보낼 데이터가 여러 개일 경우 &로 구분하여 작성합니다.
-
-                osw.write(sendMsg);//OutputStreamWriter에 담아 전송합니다.
-                osw.flush();
-                //jsp와 통신이 정상적으로 되었을 때 할 코드들입니다.
-                if(conn.getResponseCode() == conn.HTTP_OK) {
-                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
-                    BufferedReader reader = new BufferedReader(tmp);
-                    StringBuffer buffer = new StringBuffer();
-                    //jsp에서 보낸 값을 받겠죠?
-                    while ((str = reader.readLine()) != null) {
-                        buffer.append(str);
-                    }
-                    receiveMsg = buffer.toString();
-
-                } else {
-                    Log.i("통신 결과", conn.getResponseCode()+"에러");
-                    // 통신이 실패했을 때 실패한 이유를 알기 위해 로그를 찍습니다.
-                }
-
-            } catch (MalformedURLException e) {
-
-                e.printStackTrace();
-
-            } catch (IOException e) {
-
-                e.printStackTrace();
+            } catch (Exception e) {
 
             }
-            //jsp로부터 받은 리턴 값입니다.
-            return receiveMsg;
+        }
+    });
+
+
+
+
+}
+
+
+class CustomTask extends AsyncTask<String, Void, String> {
+    String sendMsg, receiveMsg;
+    @SuppressLint("WrongThread")
+    @Override
+    // doInBackground의 매개값이 문자열 배열인데요. 보낼 값이 여러개일 경우를 위해 배열로 합니다.
+    protected String doInBackground(String... strings) {
+        try {
+            String str;
+            URL url = new URL("http://182.210.78.154:8080/RegisterGps.jsp");//보낼 jsp 주소를 ""안에 작성합니다.
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");//데이터를 POST 방식으로 전송합니다.
+            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+            sendMsg = "&userID="+strings[0]+"&userPassword="+strings[1]+"&userName="+strings[2]+"&userAge="+strings[3]+"&userSex="+strings[4]+"&userPhone="+strings[5]+"&userMail="+strings[6]+"&gpsLati="+strings[7]+"&gpsLong="+strings[8];
+
+            //보낼 정보인데요. GET방식으로 작성합니다. ex) "id=rain483&pwd=1234";
+            //회원가입처럼 보낼 데이터가 여러 개일 경우 &로 구분하여 작성합니다.
+
+            osw.write(sendMsg);//OutputStreamWriter에 담아 전송합니다.
+            osw.flush();
+            //jsp와 통신이 정상적으로 되었을 때 할 코드들입니다.
+            if(conn.getResponseCode() == conn.HTTP_OK) {
+                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                BufferedReader reader = new BufferedReader(tmp);
+                StringBuffer buffer = new StringBuffer();
+                //jsp에서 보낸 값을 받겠죠?
+                while ((str = reader.readLine()) != null) {
+                    buffer.append(str);
+                }
+                receiveMsg = buffer.toString();
+
+            } else {
+                Log.i("통신 결과", conn.getResponseCode()+"에러");
+                // 통신이 실패했을 때 실패한 이유를 알기 위해 로그를 찍습니다.
+            }
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
 
         }
+        //jsp로부터 받은 리턴 값입니다.
+        return receiveMsg;
 
     }
+
+}
 
 
 }
--- Gil/AguideR Ver 0.0.4/app/src/main/java/com/hanium/AguideR/Login.java
+++ Gil/AguideR Ver 0.0.4/app/src/main/java/com/hanium/AguideR/Login.java
@@ -1,11 +1,14 @@
 package com.hanium.AguideR;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,6 +31,14 @@ import com.kakao.usermgmt.response.MeV2Response;
 import com.kakao.util.OptionalBoolean;
 import com.kakao.util.exception.KakaoException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 
 
@@ -60,8 +71,8 @@ public class Login extends AppCompatActivity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        final EditText id = (EditText) findViewById(R.id.travelid);
-        final EditText pw = (EditText) findViewById(R.id.password);
+        final EditText loginid = (EditText) findViewById(R.id.travelid);
+        final EditText loginpw = (EditText) findViewById(R.id.password);
         Button loginbutton = (Button) findViewById(R.id.loginbtn);
         final Button registerbtn = (Button) findViewById(R.id.registerbtn);
 
@@ -112,45 +123,41 @@ public class Login extends AppCompatActivity implements OnClickListener{
         });
         ///로그인 완료라서 메인 페이지 이동 시범
 
-        loginbutton.setOnClickListener(new OnClickListener() {
+        loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String userID = id.getText().toString();
-                final String userPassword = pw.getText().toString();
-
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try{
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if(success) {
-                                String userID = jsonResponse.getString("userID");
-                                String userPassword = jsonResponse.getString("userPassword");
-                                Intent intent = new Intent(Login.this, Main.class);
-                                intent.putExtra("userID", userID);
-                                intent.putExtra("userPassword", userPassword);
-                            }
-                            else{
-                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
-                                builder.setMessage("로그인에 실패하였습니다.")
-                                        .setNegativeButton("다시 시도", null)
-                                        .create()
-                                        .show();
-                            }
-
-                        } catch (Exception e)
-                        {
-                            e.printStackTrace();
+                String userID = loginid.getText().toString();
+                String userPassword = loginpw.getText().toString();
 
 
-                        }
+                try {
+                    String result;
+                    LoginTask task = new LoginTask();
+                    result = task.execute(userID, userPassword).get();
+                    Log.i("리턴 값", result);
+
+                    if(result.equals("true")){
+                        Intent maiintent = new Intent(Login.this, Main.class);
+                        Login.this.startActivity(maiintent);
+
+                        finish();
+
+                        //true 받으면 메인 페이지로 이동하기
+
+
+                    } if(result.equals("false")) {
+
+                        Toast.makeText(getApplicationContext(), "로그인에 실패 하였습니다.", Toast.LENGTH_SHORT).show();
+                        finish();
+
+                    } if(result.equals("noId")) {
+                        Toast.makeText(getApplicationContext(), "아이디가 존재하지 않습니다.", Toast.LENGTH_SHORT).show();
+                        finish();
                     }
-                };
 
-                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(Login.this);
-                queue.add(loginRequest);
+                } catch (Exception e) {
+
+                }
             }
         });
 
@@ -364,6 +371,60 @@ public class Login extends AppCompatActivity implements OnClickListener{
         public void onSessionOpenFailed(KakaoException e) { //세션을 여는 도중 오류가 발생한 경우 -> Toast 메세지를 띄움.
             Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: "+e.toString(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    class LoginTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+        @SuppressLint("WrongThread")
+        @Override
+        // doInBackground의 매개값이 문자열 배열인데요. 보낼 값이 여러개일 경우를 위해 배열로 합니다.
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
+                URL url = new URL("http://182.210.78.154:8080/Login.jsp");//보낼 jsp 주소를 ""안에 작성합니다.
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");//데이터를 POST 방식으로 전송합니다.
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "&userID="+strings[0]+"&userPassword="+strings[1];
+
+                //보낼 정보인데요. GET방식으로 작성합니다. ex) "id=rain483&pwd=1234";
+                //회원가입처럼 보낼 데이터가 여러 개일 경우 &로 구분하여 작성합니다.
+
+                osw.write(sendMsg);//OutputStreamWriter에 담아 전송합니다.
+                osw.flush();
+                //jsp와 통신이 정상적으로 되었을 때 할 코드들입니다.
+                if(conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    //jsp에서 보낸 값을 받겠죠?
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+
+                } else {
+                    Log.i("통신 결과", conn.getResponseCode()+"에러");
+                    // 통신이 실패했을 때 실패한 이유를 알기 위해 로그를 찍습니다.
+                }
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            //jsp로부터 받은 리턴 값입니다.
+            return receiveMsg;
+
+        }
+
     }
 
 }
